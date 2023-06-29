8418bbbcf5beda620224b363ad68ab97832cbbe8
v1ncii <134945156+v1ncii@users.noreply.github.com>
2023-06-29 18:18:41 +0200

    Update Tshediso.js

--- pages/Tshediso.js
+++ pages/Tshediso.js
@@ -1,10 +1,1 @@
-import Link from "next/link"
 
-export default function Home() {
-    return <div>
-     <p>Tshediso is cool</p>
-     <link href="/">
-        <a>Back</a>
-     </link>
-    </div>
-}
